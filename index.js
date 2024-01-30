3ef2a70b30159b0a95ff39ca219e09ff29c3848a
han18 <han.fadah@gmail.com>
2024-01-30 11:16:40 -0500

    created the files and database connection

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ import "./loadEnv.js";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import moviesRouter from "./routes/movies.js";
 
 console.log(process.env.ATLAS_URI);
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// middlewear
+// middleware
 app.use(cors()); // allows frontend to connect to backend
 app.use(morgan("dev")); // logger
 app.use(express.json()); // for data in req.body
 app.use(express.urlencoded({ extended: true })); // allow data in url string
+
+// using the movies router
+app.use("/api/movies", moviesRouter);
 
 app.get("/", (req, res) => {
   res.send("backend...");
